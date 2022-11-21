24c82723c3211309ba9ae8e127a2286ae588ba43
gabrieeel <gabrielp@gmail.com>
2022-11-20 22:53:10 -0300

    update METADATA_URL

--- hardhat-nft-collection/constants/index.js
+++ hardhat-nft-collection/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xd5C8bf512F127303E55F67A1ed2Ab650b85E1aa9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-eight-iota.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
